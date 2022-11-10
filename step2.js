a0765355e2d584308be7d2bf0ee0837898d6a6dd
Eric Doering <ericbenjamindoering@gmail.com>
2022-11-09 19:27:07 -0500

    exercise submission

---
+++ step2.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const process = require("process");
+const axios = require("axios");
+
+function cat(path){
+    fs.readFile(path, "utf8", function(err, data){
+        if(err){
+            console.error(`Error reading ${path}: ${err}`);
+            process.exit(1);
+        }
+        else {
+            console.log(data);
+        }
+    });
+}
+
+
+async function webCat(url) {
+    try {
+        let response = await axios.get(url);
+        console.log(response.data.body);
+    }
+    catch (err) {
+        console.error(`Error fetching ${url}: ${err}`);
+        process.exit(1);
+    }
+}
+
+let path = "http://google.com"
+
+if (path.slice(0,4) === "http") {
+    webCat(path);
+}
+else {
+    cat(path)
+}
